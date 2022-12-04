72438a669610b33c1ed0ebe8b52ff7cc121f9407
Garrett Moody <extrillieos@gmail.com>
2022-12-03 21:55:45 -0800

    cleand up day4

--- Day4/solution.py
+++ Day4/solution.py
@@ -11,7 +11,7 @@ def part1():
             elf1, elf2 = line.split(',')
             elf1 = elf1.split('-')
             elf2 = elf2.split('-')
-       
+            
             if int(elf1[0]) <= int(elf2[0]) and int(elf1[1]) >= int(elf2[1]):
                 prio += 1
             elif int(elf2[0]) <= int(elf1[0]) and int(elf2[1]) >= int(elf1[1]):
