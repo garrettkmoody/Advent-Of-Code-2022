1b08ba303dfd16aab993440d5f97dc1151f44e48
Garrett Moody <extrillieos@gmail.com>
2022-12-05 21:36:15 -0800

    with set

--- Day6/solution.py
+++ Day6/solution.py
@@ -2,51 +2,48 @@ def main():
     part1()
     part2()
 
+
 def part1():
 
     with open('input.txt') as f:
         line = f.readline()
-      
-        uniqueSet = list(line[:3])
-        for i in range(3, len(line)):
-            uniqueSet.append(line[i])
-            done = False
-            for j in range(len(uniqueSet)):
-                if done:
-                    break
-                for e in range(j + 1, len(uniqueSet)):
-                    if uniqueSet[j] == uniqueSet[e]:
-                        uniqueSet.pop(0)
-                        done = True
-                        break
-    
-            if done == False:
-                print("Solution to Part 1: {}".format(i + 1))
+
+        uniqueSet = set([])
+        endChar = None
+        for i in range(len(line)):
+            uniqueSet.add(line[i])
+            for j in range(i, i + 4):
+                uniqueSet.add(line[j])
+
+            if len(uniqueSet) == 4:
+                endChar = i + 4
                 break
-   
+            else:
+                uniqueSet.clear()
+
+    print("Solution to Part 1: {}".format(endChar))
+
 
 def part2():
-
     with open('input.txt') as f:
         line = f.readline()
-        uniqueSet = list(line[:13])
-    
-        for i in range(3, len(line)):
-            uniqueSet.append(line[i])
-            done = False
-            for j in range(len(uniqueSet)):
-                if done:
-                    break
-                for e in range(j + 1, len(uniqueSet)):
-                    if uniqueSet[j] == uniqueSet[e]:
-                        uniqueSet.pop(0)
-                        done = True
-                        break
-            
-            if done == False:
-                print("Solution to Part 2: {}".format(i + 1))
+
+        uniqueSet = set([])
+        endChar = None
+        for i in range(len(line)):
+            uniqueSet.add(line[i])
+
+            for j in range(i, i + 14):
+                uniqueSet.add(line[j])
+
+            if len(uniqueSet) == 14:
+                endChar = i + 14
                 break
-    
+            else:
+                uniqueSet.clear()
+
+    print("Solution to Part 2: {}".format(endChar))
+
 
 if __name__ == '__main__':
     main()
