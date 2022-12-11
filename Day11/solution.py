4a24f4248dc5f7274eb58a535983296aa5077ad8
Garrett Moody <extrillieos@gmail.com>
2022-12-10 23:27:56 -0800

    remove unneccesary code

--- Day11/solution.py
+++ Day11/solution.py
@@ -32,21 +32,14 @@ class Monkey:
                 item *= int(self.operation[1])
 
         if not self.part2:
-
             item = item // 3
-        else:
-
-            overFlow = len(str(item)) - 7
+        else:    
+            overFlow = len(str(item)) - len(str(specialNumber))
 
             while overFlow > 0:
-
                 multiplyNumber = int(str(item)[0:overFlow])
                 item -= (specialNumber * multiplyNumber)
-
-                overFlow = len(str(item)) - 7
-            
-        
-        
+                overFlow = len(str(item)) - len(str(specialNumber))
 
         if item % self.test == 0:
             return (item, self.monkey_true)
@@ -78,7 +71,6 @@ def part1():
 
         for _ in range(20): 
             for monkey in monkeys:
-              
                 while True:
                     info = monkey.inspect()
                     if info == -1:
@@ -87,9 +79,7 @@ def part1():
                     monkeys[location].items.append(item)  
         max1 = 0
         max2 = 0
-        count = 0
         for monkey in monkeys:
-            count += 1
             if monkey.inspectionCount > max1:
                 max2 = max1
                 max1 = monkey.inspectionCount
@@ -136,9 +126,7 @@ def part2():
                     monkeys[location].items.append(item)
         max1 = 0
         max2 = 0
-        count = 0
         for monkey in monkeys:
-            count += 1
             if monkey.inspectionCount > max1:
                 max2 = max1
                 max1 = monkey.inspectionCount
